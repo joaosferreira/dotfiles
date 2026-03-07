c176284312c21d73cf3d7545dc52ae5ce755d461
João Ferreira <8233411+joaosferreira@users.noreply.github.com>
2026-03-07 19:57:54 +0000

    Add Javascript and CSS parsers

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,8 +3,14 @@ return {
   lazy = false,
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter").install({ "lua", "vim", "vimdoc", "bash" })
-
+    require("nvim-treesitter").install({
+      "lua",
+      "vim",
+      "vimdoc",
+      "bash",
+      "javascript",
+      "css"
+    })
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "*",
       callback = function() vim.treesitter.start() end,
