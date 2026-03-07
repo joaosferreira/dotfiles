7787410e79c177de170090da2b6536e754f892db
João Ferreira <8233411+joaosferreira@users.noreply.github.com>
2026-03-07 20:08:34 +0000

    Fix treesitter error on unsupported filetypes

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -13,7 +13,7 @@ return {
     })
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "*",
-      callback = function() vim.treesitter.start() end,
+      callback = function() pcall(vim.treesitter.start) end,
     })
   end,
 }
